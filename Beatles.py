22200069c1fd88f7054e5009e25ad0ee1918455e
David Stroud <david@greymatter-analytics.com>
2017-07-19 08:54:01 -0500

    parse_file method is working properly

--- Beatles.py
+++ Beatles.py
@@ -15,9 +15,21 @@ DATAFILE = "beatles-diskography.csv"
 
 def parse_file(datafile):
     data = []
-    with open(datafile, "rb") as f:
-
+    with open(datafile, "r") as f:
+        header = f.readline().split(",")
+        counter = 0
         for line in f:
+            if counter == 10:
+                break
+
+            fields = line.split(",")
+            entry = {}
+
+            for i, value in enumerate(fields):
+                entry[header[i].strip()] = value.strip()
+
+            data.append(entry)
+            counter += 1
             print(line)
 
     return data
